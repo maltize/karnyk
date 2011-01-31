9096694d769116cf0286e53f1a277b517d5cde89
widlak <widlak@bwidlak.home>
2011-01-31 21:15:13 +0000

    added to view checkbox - terms condition

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,7 @@ class Message < ActiveRecord::Base
   validates_length_of :name, :minimum => 3
   validates_length_of :target_name, :minimum => 3
   validates_length_of :body, :minimum => 5
+  validates_presence_of :terms_conditions
 
   validates_format_of :target_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'Nieprawidłowy zapis email'
 
