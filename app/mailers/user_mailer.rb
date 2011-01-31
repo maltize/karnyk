d861c237e93fb7aa8591c910187a0b3b770647e0
widlak <widlak@jsavage-pc.wilde.local>
2011-01-31 15:48:52 +0000

    user mailer added bcc at info@karny..

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,11 @@ class UserMailer < ActionMailer::Base
 
   def notify(message)
     @message = message
-    mail(:from => SYSTEM_EMAIL, :to => message.target_email,
-      :subject => "Karny Kutas - od kogo dostałeś?")
+    mail(:from => SYSTEM_EMAIL, :to => message.target_email, :bcc => ENV['BCC_EMAIL'],
+      :subject => "Otrzymujesz Karnego Kutasa?")
   end
 
+  def attach_logo
+    attachments.inline['kk_logo.gif'] = File.read("#{Rails.root}/public/images/email_logo.gif")    
+  end
 end
